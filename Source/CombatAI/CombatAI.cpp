51c9c0e211fa1ea755dcf7fae3c623484eed44ca
u1429721 <chris.y.zhong@utah.edu>
2024-02-23 19:44:32 -0700

    Initial commit

---
+++ Source/CombatAI/CombatAI.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CombatAI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CombatAI, "CombatAI" );
+ 
